43039773287decc5e433c61f8d6df6048272ffe5
pwfee <vinsonring@gmail.com>
2018-05-15 12:31:06 +0800

    [A] 添加 FaceInfo Model & ImageModel 新增 status 字段

--- utils/models.py
+++ utils/models.py
@@ -11,5 +11,10 @@ class ImageUploadForm(forms.Form):
 class ImageModel(models.Model):
   file_name = models.CharField(max_length=100)
   created_at = models.DateTimeField(auto_now_add=True)
+  status = models.IntegerField(default=0)
 
 
+class FaceInfo(models.Model):
+  image = models.ForeignKey('ImageModel')
+  face_score = models.FloatField(default=0)
+  like_count = models.PositiveIntegerField(default=0)
